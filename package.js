5d6ce1ef31b15a81eac753a873380c7461720fa8
Andrew Reedy <andrew.reedy@thecontrolgroup.com>
2014-04-11 05:29:22 -0700

    update

--- package.js
+++ package.js
@@ -4,6 +4,7 @@ Package.describe({
 
 Package.on_use(function (api, where) {
   api.use(['ui',  'underscore', 'deps', 'templating'], 'client');
+  
   api.use('coffeescript',['client']);
   api.add_files(['tabs.coffee'], 'client');
 });
